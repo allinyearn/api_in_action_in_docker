588cfc229ba33861a30a3236f4e1e774b90f9fe0
Александр <ruyearn@gmail.com>
2022-02-25 13:23:39 +0300

    fix debug

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='SUP3R-S3CR3T-K3Y-F0R-MY-PR0J')
 
-if type(os.getenv('DEBUG')) == 'NoneType':
+if type(os.getenv('DEBUG')) == 'None':
     DEBUG = False
 else:
     DEBUG = os.getenv('DEBUG').lower() in ('on', 'true', '1')
